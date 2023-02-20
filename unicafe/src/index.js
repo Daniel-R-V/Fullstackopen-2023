e5592ed9320285b92f68b65b6bce2b10d29b6ad2
Daniel Ruiz <danirvicente@gmail.com>
2023-02-20 09:56:57 +0100

    step5

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -1,30 +1,72 @@
 import React, { useState } from "react"
 import ReactDOM from "react-dom"
 
+const Button = ({ handleGoodClick, handleNeutralClick, handleBadClick }) => {
+    return (
+        <>
+            <button onClick={handleGoodClick}>good</button>
+            <button onClick={handleNeutralClick}>neutral</button>
+            <button onClick={handleBadClick}>bad</button>
+        </>
+    )
+}
+
+const Statistic = ({ text, value }) => {
+    return (
+        <h3>
+            {text} {value}
+        </h3>
+    )
+}
+
+const Statistics = ({ good, neutral, bad }) => {
+    const all = good + neutral + bad
+    const average = (good - bad) / all
+    const positive = (good / all) * 100
+
+    return all === 0 ? (
+        <>
+            <h1>statistics</h1>
+            <p>No feedback given</p>
+        </>
+    ) : (
+        <>
+            <h1>statistics</h1>
+
+            <Statistic text="good" value={good} />
+            <Statistic text="neutral" value={neutral} />
+            <Statistic text="bad" value={bad} />
+
+            <h3>all {all}</h3>
+            <h3>average {average}</h3>
+            <h3>positive {positive}%</h3>
+        </>
+    )
+}
+
 const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
-    const all = good + neutral + bad
-    const average = (good - bad) / all
-    const positive = (good / all) * 100
+
+    const handleGoodClick = () => {
+        setGood(good + 1)
+    }
+
+    const handleNeutralClick = () => {
+        setNeutral(neutral + 1)
+    }
+
+    const handleBadClick = () => {
+        setBad(bad + 1)
+    }
 
     return (
         <div>
             <h1>give feedback</h1>
 
-            <button onClick={() => setGood(good + 1)}>good</button>
-            <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-            <button onClick={() => setBad(bad + 1)}>bad</button>
-
-            <h1>statistics</h1>
-
-            <h3>good {good}</h3>
-            <h3>neutral {neutral}</h3>
-            <h3>bad {bad}</h3>
-            <h3>all {all}</h3>
-            <h3>average {average}</h3>
-            <h3>positive {positive}%</h3>
+            <Button handleGoodClick={handleGoodClick} handleNeutralClick={handleNeutralClick} handleBadClick={handleBadClick} />
+            <Statistics good={good} neutral={neutral} bad={bad} />
         </div>
     )
 }
