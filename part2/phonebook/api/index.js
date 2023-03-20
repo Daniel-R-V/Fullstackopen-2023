24c6c2e3feb2838e62c1786d7939dbd1639dfc96
Daniel Ruiz <danirvicente@gmail.com>
2023-03-20 16:04:22 +0100

    Request Logger

--- part2/phonebook/api/index.js
+++ part2/phonebook/api/index.js
@@ -1,10 +1,20 @@
 const express = require("express")
 const app = express()
 
-app.use(express.json())
+const requestLogger = (request, response, next) => {
+    console.log("Method:", request.method)
+    console.log("Path:  ", request.path)
+    console.log("Body:  ", request.body)
+    console.log("---")
+    next()
+}
+
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: "unknown endpoint" })
 }
+
+app.use(express.json())
+app.use(requestLogger)
 
 let persons = [
     {
