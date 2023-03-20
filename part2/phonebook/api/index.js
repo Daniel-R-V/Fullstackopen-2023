de39139c51b2f666d0e2ddf83a088f9290b471b0
Daniel Ruiz <danirvicente@gmail.com>
2023-03-20 15:59:37 +0100

    unknown endpoint

--- part2/phonebook/api/index.js
+++ part2/phonebook/api/index.js
@@ -88,6 +88,12 @@ app.post("/api/persons", (request, response) => {
     response.json(person)
 })
 
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: "unknown endpoint" })
+}
+
+app.use(unknownEndpoint)
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
