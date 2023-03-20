d169f94d30cd43e00794af8fc74c2598fe826852
Daniel Ruiz <danirvicente@gmail.com>
2023-03-20 14:00:15 +0100

    task 3.5

--- part2/phonebook/api/index.js
+++ part2/phonebook/api/index.js
@@ -60,6 +60,20 @@ app.delete("/api/persons/:id", (request, response) => {
     response.status(204).end()
 })
 
+app.post("/api/persons", (request, response) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: Math.floor(Math.random() * 100)
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
