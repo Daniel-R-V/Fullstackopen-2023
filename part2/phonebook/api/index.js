3a5b7f1639c8337629e734fe6e96c7d921dfab12
Daniel Ruiz <danirvicente@gmail.com>
2023-03-20 16:41:01 +0100

    add morgan to register logs

--- part2/phonebook/api/index.js
+++ part2/phonebook/api/index.js
@@ -1,20 +1,13 @@
 const express = require("express")
+var morgan = require("morgan")
 const app = express()
-
-const requestLogger = (request, response, next) => {
-    console.log("Method:", request.method)
-    console.log("Path:  ", request.path)
-    console.log("Body:  ", request.body)
-    console.log("---")
-    next()
-}
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: "unknown endpoint" })
 }
 
 app.use(express.json())
-app.use(requestLogger)
+app.use(morgan(":method :url :status :response-time ms - :res[content-length]"))
 
 let persons = [
     {
