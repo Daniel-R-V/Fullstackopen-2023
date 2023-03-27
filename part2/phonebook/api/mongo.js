35ba693f45f65513f8c2a4fda3648e01d3b714f1
Daniel Ruiz <danirvicente@gmail.com>
2023-03-27 16:29:45 +0200

    new database in phonebook

--- part2/phonebook/api/mongo.js
+++ part2/phonebook/api/mongo.js
@@ -7,32 +7,34 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://Dani:${password}@cluster0.esehfxk.mongodb.net/note-app?retryWrites=true&w=majority`
+const url = `mongodb+srv://Dani:${password}@cluster0.esehfxk.mongodb.net/phone-book?retryWrites=true&w=majority`
 
 mongoose.connect(url)
 
-const noteSchema = new mongoose.Schema({
-    content: String,
-    date: Date,
-    important: Boolean
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
 })
 
-const Note = mongoose.model("Note", noteSchema)
+const Person = mongoose.model("Person", personSchema)
 
-// const note = new Note({
-//     content: "HTML is easy",
-//     date: new Date(),
-//     important: true
-// })
+const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4]
+})
 
-// note.save().then((result) => {
-//     console.log("note saved!")
-//     mongoose.connection.close()
-// })
-
-Note.find({}).then((result) => {
-    result.forEach((note) => {
-        console.log(note)
+if (process.argv[3] && process.argv[4]) {
+    person.save().then((result) => {
+        console.log(`added ${result.name} number ${result.number} to phonebook`)
+        mongoose.connection.close()
     })
-    mongoose.connection.close()
-})
+} else {
+    Person.find({})
+        .then(console.log("phonebook:"))
+        .then((result) => {
+            result.forEach((person) => {
+                console.log(`${person.name} ${person.number}`)
+            })
+            mongoose.connection.close()
+        })
+}
