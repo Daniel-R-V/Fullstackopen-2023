a6261f06f0206ed20264afd0d1d98b813ac2d9e9
Daniel Ruiz <danirvicente@gmail.com>
2023-03-30 18:10:12 +0200

    fix errors

--- part2/phonebook/api/controllers/people.js
+++ part2/phonebook/api/controllers/people.js
@@ -2,16 +2,16 @@ const peopleRouter = require("express").Router()
 const Person = require("../models/person")
 
 peopleRouter.get("/", (request, response) => {
-    Person.find({}).then((people) => {
-        response.json(people)
+    Person.find({}).then((person) => {
+        response.json(person)
     })
 })
 
 peopleRouter.get("/:id", (request, response, next) => {
     Person.findById(request.params.id)
-        .then((note) => {
-            if (note) {
-                response.json(note)
+        .then((person) => {
+            if (person) {
+                response.json(person)
             } else {
                 response.status(404).end()
             }
@@ -22,15 +22,15 @@ peopleRouter.get("/:id", (request, response, next) => {
 peopleRouter.post("/", (request, response, next) => {
     const body = request.body
 
-    const note = new Person({
-        content: body.content,
-        important: body.important || false,
-        date: new Date()
+    const person = new Person({
+        name: body.name,
+        number: body.number
     })
 
-    note.save()
-        .then((savedNote) => {
-            response.json(savedNote)
+    person
+        .save()
+        .then((savedPerson) => {
+            response.json(savedPerson)
         })
         .catch((error) => next(error))
 })
@@ -46,12 +46,12 @@ peopleRouter.delete("/:id", (request, response, next) => {
 peopleRouter.put("/:id", (request, response, next) => {
     const body = request.body
 
-    const note = {
-        content: body.content,
-        important: body.important
+    const person = {
+        name: body.name,
+        number: body.number
     }
 
-    Person.findByIdAndUpdate(request.params.id, note, { new: true })
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
         .then((updatedNote) => {
             response.json(updatedNote)
         })
